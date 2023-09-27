d56af3aa8543612a4df7afd5b2841984b6a2414a
ejcanoy <euan.james.canoy@gmail.com>
2023-09-27 15:22:01 -0700

    cleaned up logic for sounds

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,16 @@
 function Card({ name, fullPortrait, handleMove, isGameOver }) {
     return (
         <>
-            <div className="w-48 h-64 min-w-[190px] bg-valorantred">
-                <button className="w-full h-full" onClick={() => handleMove(name)} disabled={isGameOver}>
+            <div className="w-48 h-64 min-w-[190px] rounded-lg bg-valorantblack text-white font-mono">
+                <button className="w-full h-full p-3" onClick={() => handleMove(name)} disabled={isGameOver}>
                     <div>{name}</div>
-                    <img
-                        src={fullPortrait}
-                        alt={name}
-                        style={{ objectFit: 'cover', height: '10rem', width: '12rem'}}
-                    />
+                    <div className="bg-valorantred rounded-lg">
+                        <img
+                            src={fullPortrait}
+                            alt={name}
+                            style={{ objectFit: 'cover', height: '12rem', width: '16rem'}}
+                        />
+                    </div>
                 </button>
             </div>
 
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import Card from "./card"
-import ReactHowler from 'react-howler';
-import { Howl, Howler } from "howler";
+import { Howl } from "howler";
 
 function Game({ playSound, gameCharacters, difficulty }) {
     const [currentScore, setCurrentScore] = useState(0);
@@ -9,9 +8,10 @@ function Game({ playSound, gameCharacters, difficulty }) {
     const [moves, setMoves] = useState(new Set());
     const [isGameOver, setIsGameOver] = useState(false);
     const wonOrLoss = moves.size + 1 === difficulty.guesses;
+    const currentLevel = currentScore === difficulty.guesses ? currentScore - 1 : currentScore; 
 
     const kill = new Howl({
-        src: [`/src/assets/valorant-kill-${currentScore + 1 % 6}.mp3`],
+        src: [`/src/assets/valorant-kill-${(currentScore) % 5 + 1}.mp3`],
         volume: 0.1
     });
 
@@ -35,16 +35,19 @@ function Game({ playSound, gameCharacters, difficulty }) {
                 death.play();
             }
             console.log("lose");
+            setBestScore(Math.max(currentScore, bestScore))
             setIsGameOver(true);
         } else if (moves.size + 1 === difficulty.levels.length) {
             if (playSound) {
                 kill.play();
             }
             console.log("won");
+            setCurrentScore(currentScore + 1);
+            setBestScore(Math.max(currentScore + 1, bestScore))
             setIsGameOver(true);
         } else {
             if (playSound) {
-            kill.play();
+                kill.play();
             }
             setCurrentScore(currentScore + 1);
             const updatedMoves = new Set(moves);
@@ -55,6 +58,7 @@ function Game({ playSound, gameCharacters, difficulty }) {
 
     return (
         <>
+
             {
                 isGameOver &&
                 <>
@@ -67,12 +71,19 @@ function Game({ playSound, gameCharacters, difficulty }) {
                     </div>
                 </>
             }
+            <header className="flex align-top flex-wrap justify-between px-4 flex-wrap w-full h-[200px]">
+                <button className="h-10"><a href="/">Home</a></button>
+                <div>
+                    <p>Score: {currentScore}</p>
+                    <p>Best Score: {bestScore}</p>
+                </div>
+            </header>
 
             {
                 gameCharacters &&
                 <>
                     <div className="flex flex-wrap w-[80%] justify-evenly mx-auto">
-                        {difficulty.levels[currentScore].map((characterIndex) => (
+                        {difficulty.levels[currentLevel].map((characterIndex) => (
                             <Card
                                 key={gameCharacters[characterIndex].displayName}
                                 name={gameCharacters[characterIndex].displayName}
